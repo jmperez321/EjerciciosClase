6d2feb47f1291ba3702c386fdbd621dfc629d9fb
jmperez321 <jmperez@elpuig.xeill.net>
2025-09-28 13:31:20 +0200

    Create Gatito.java

---
+++ ejercicios/src/Gatito.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("(\\___/)");
+        System.out.println("(=` .`)");
+        System.out.println("(_(\")(\")");
+    }
+}
