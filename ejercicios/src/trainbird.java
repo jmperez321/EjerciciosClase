ecf161ec8f2e1f318aeba1bb166438d8b820c0af
jmperez <jmperez@elpuig.xeill.net>
2025-10-02 19:18:23 +0200

    +Trainbird

---
+++ ejercicios/src/trainbird.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class trainbird {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        float Dt = sc.nextFloat();
+        float Vt = sc.nextFloat();
+        float Vb = sc.nextFloat();
+
+        // float Tt = Vt/Dt;
+        float Db = Dt/Vt*Vb;
+
+        System.out.println(Db);
+    }
+}
