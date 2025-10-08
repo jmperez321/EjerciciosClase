ca2a9c4c3b15bb7e616378a1b38a34637a031107
dam1 <ircabla@elpuig.xeill.net>
2025-10-08 17:14:46 +0200

    + ejr

--- ejercicios/src/NumSENAR.java
+++ ejercicios/src/NumSENAR.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class NumPar {
+public class NumSENAR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
@@ -10,11 +10,11 @@ public class NumPar {
         int B = sc.nextInt();
         int C = sc.nextInt();
 
-        Boolean Ax = (A % 2) ==0;
-        Boolean Bx = (B % 2) ==0;
-        Boolean Cx = (C % 2) ==0;
+        Boolean Ax = (A % 2) >0;
+        Boolean Bx = (B % 2) >0;
+        Boolean Cx = (C % 2) >0;
 
-        Boolean RES = (Ax && !Bx && !Cx) || (!Ax && Bx && !Cx) || (!Ax && !Bx && Cx);
+        Boolean RES = (Ax && Bx && Cx)||(!Ax && !Bx && !Cx);
         System.out.println(RES);
 
     }
