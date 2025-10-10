5a7567bbb5d8de4d6a083e2e4034ada5b51bf23a
jmperez <jmperez@elpuig.xeill.net>
2025-10-10 13:58:52 +0200

    s

--- ejercicios/src/Murcianitos.java
+++ ejercicios/src/Murcianitos.java
@@ -6,12 +6,16 @@ public class Murcianitos {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        int X0 = sc.nextInt();
-        int Y0 = sc.nextInt();
         int X1 = sc.nextInt();
         int Y1 = sc.nextInt();
+        int W1 = sc.nextInt();
+        int H1 = sc.nextInt();
         int X2 = sc.nextInt();
         int Y2 = sc.nextInt();
+        int W2 = sc.nextInt();
+        int H2 = sc.nextInt();
 
-
+        boolean pum = !((X1 + W1 <= X2) || (X2 + W2 <= X1) || (Y1 + H1 <= Y2) || (Y2 + H2 <= Y1));
+        System.out.println(pum);
+    }
 }
