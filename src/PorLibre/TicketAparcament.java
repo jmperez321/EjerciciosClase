649d5c44b6f3cc020ad8d8e420a7d3fbbae0d210
dam1 <ircabla@elpuig.xeill.net>
2025-10-10 17:48:10 +0200

    Separado en multiples variables para comprobar el debug

---
+++ src/PorLibre/TicketAparcament.java
@@ -0,0 +1,19 @@
+package PorLibre;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TicketAparcament {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        int HH = sc.nextInt();
+        int MM = sc.nextInt();
+        int SS = sc.nextInt();
+        float TAR = sc.nextFloat(); // tarifa minutos/precio.
+        float DIN = sc.nextFloat(); // Indica la cantidad de dineros introducidos.
+
+
+    }
+}
