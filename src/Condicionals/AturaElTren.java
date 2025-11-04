6054cefdbc5fab655ed727ab8ccc1b18ec8a3589
dam1 <ircabla@elpuig.xeill.net>
2025-11-04 19:12:42 +0100

    +3 ej trminados. Creado el AturaElTren pero sin hacer.

---
+++ src/Condicionals/AturaElTren.java
@@ -0,0 +1,13 @@
+package Condicionals;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AturaElTren {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+
+    }
+}
--- src/Condicionals/ControlFerroviari.java
+++ src/Condicionals/ControlFerroviari.java
@@ -8,7 +8,18 @@ public class ControlFerroviari {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
+        String S = sc.nextLine();
+        boolean F = sc.nextBoolean();
+        sc.nextLine();
+        int C = sc.nextInt();
 
-
+        System.out.println("Estat del semàfor (vermell, ambar, verd):");
+        System.out.println("Fre d'emergència? (true, false):");
+        System.out.println("Ordres del centre de control (0, 1, 2):");
+        if (( S.equals("verd") || S.equals("àmbar"))  && !F && C==0) {
+            System.out.println("El tren pot continuar");
+        } else {
+            System.out.println("El tren s'ha d'aturar!!");
+        }
     }
 }
