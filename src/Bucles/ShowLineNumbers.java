06892ce88c885ee2a0e07f9645e5a3ceaa501dae
jmperez <jmperez@elpuig.xeill.net>
2025-11-16 14:03:46 +0000

    subido 2

---
+++ src/Bucles/ShowLineNumbers.java
@@ -0,0 +1,20 @@
+package Bucles;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ShowLineNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+
+        int LineNum = 1;
+        String Line = sc.nextLine();
+        while (!Line.equals("END")){
+            System.out.println(LineNum +" | "+ Line);
+            Line = sc.nextLine();
+            LineNum++;
+        }
+    }
+}
--- src/Bucles/SumadeParellsIProducsImp.java
+++ src/Bucles/SumadeParellsIProducsImp.java
@@ -8,20 +8,22 @@ public class SumadeParellsIProducsImp {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        int Num1 = sc.nextInt();
-        int Num2 = sc.nextInt();
 
-        for (int A = Num1;A>=Num2;A--){
-            if (A%3==0 && A%5==0){
-                System.out.println("FizzBuzz");
-            } else if (A%3==0) {
-                System.out.println("Fizz");
-            } else if (A%5==0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(A);
+        int Num = sc.nextInt();
+        int Parell = 0;
+        int Impar = 1;
+        int NumTotals = 0;
+        while (Num!=-1) {
+            if (Num % 2 == 0) {
+                Parell +=Num;
+            } else if (Num%2!=0){
+                Impar *= Num;
             }
+            NumTotals +=1;
+            Num = sc.nextInt();
         }
+        System.out.println(Parell);
+        System.out.println(Impar);
+        System.out.println(NumTotals);
     }
 }
-
