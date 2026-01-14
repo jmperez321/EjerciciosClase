0a0dfac71ed4ade756f2409413b874a6d0bb4261
dam1 <americanafriadominguez@elpuig.xeill.net>
2026-01-14 19:09:58 +0100

    sin terminar aprovarElModul

---
+++ src/Metodoss/aprovarElModul.java
@@ -0,0 +1,32 @@
+package Metodoss;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aprovarElModul {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        int[] nUF1 = new int[3];
+        for (int i = 0; i < 3; i++) {
+            nUF1[i] = sc.nextInt();
+        }
+        int[] nUF2 = new int[3];
+        for (int i = 0; i < 3; i++) {
+            nUF2[i] = sc.nextInt();
+        }
+        int[] nUF3 = new int[3];
+        for (int i = 0; i < 3; i++) {
+            nUF3[i] = sc.nextInt();
+        }
+        Boolean aUF1 = false;
+
+        if (((nUF1[1]/nUF1[0]*100)>=75)&&nUF1[3]>4)|| )
+
+        }
+
+
+    }
+}
+
