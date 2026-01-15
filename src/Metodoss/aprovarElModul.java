a11272ff7b7b06825f873416c8dd68721212b8b6
dam1 <americanafriadominguez@elpuig.xeill.net>
2026-01-15 18:37:58 +0100

    Metodo ejemplo Profe.

--- src/Metodoss/aprovarElModul.java
+++ src/Metodoss/aprovarElModul.java
@@ -8,25 +8,29 @@ public class aprovarElModul {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        int[] nUF1 = new int[3];
-        for (int i = 0; i < 3; i++) {
-            nUF1[i] = sc.nextInt();
-        }
-        int[] nUF2 = new int[3];
-        for (int i = 0; i < 3; i++) {
-            nUF2[i] = sc.nextInt();
-        }
-        int[] nUF3 = new int[3];
-        for (int i = 0; i < 3; i++) {
-            nUF3[i] = sc.nextInt();
-        }
-        Boolean aUF1 = false;
-
-        if (((nUF1[1]/nUF1[0]*100)>=75)&&nUF1[3]>4)|| )
-
-        }
-
-
+        boolean aprovUF1 = aprovaUF1(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        boolean aprovUF2 = aprovaUF2(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        boolean aprovUF3 = aprovaUF3(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        System.out.println(aprovUF1 && aprovUF2 && aprovUF3);
     }
+
+    public static boolean aprovaUF1(int pt, int pl, int ne) {
+        boolean via1UF1 = (pl / (float) pt >= 0.75f) && ne >= 4;
+        boolean via2UF1 = (pl / (float) pt >= 0.5f) && ne >= 5;
+        boolean via3UF1 = ne >= 7;
+        return (via1UF1 || via2UF1 || via3UF1);
+    }
+
+    public static boolean aprovaUF2(int pt, int pl, int ne) {
+        boolean via1UF2 = (pl / (float) pt >= 0.75f) && ne >= 4;
+        boolean via2UF2 = (pl == pt);
+        boolean via3UF2 = (pl / (float) pt >= 0.5f) && ne >= 5;
+        return (via1UF2 || via2UF2 || via3UF2);
+    }
+
+    public static boolean aprovaUF3(int pt, int pl, int ne) {
+        return (pl == pt && ne >= 5);
+    }
+
 }
 
