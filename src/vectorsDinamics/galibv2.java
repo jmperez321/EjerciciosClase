304ef73a9362d7ce2d3a6b1f96ee8720b836cff5
jmperez <jmperez@elpuig.xeill.net>
2026-02-18 17:56:05 +0100

    inicio ej vectores

---
+++ src/vectorsDinamics/galibv2.java
@@ -0,0 +1,22 @@
+package vectorsDinamics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class galibv2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        List<Float> liada = new ArrayList<>();
+        do{
+            float melon = sc.nextFloat();
+            liada.add(melon);
+
+        }while(!melon =-1);
+
+    }
+}
