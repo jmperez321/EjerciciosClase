7d9bb793eac1f87915b8854c9dae304d8ba96462
jmperez <jmperez@elpuig.xeill.net>
2026-02-19 12:44:30 +0000

    /vectoresDinamicos/gablibv2.java terminado

--- src/vectorsDinamics/galibv2.java
+++ src/vectorsDinamics/galibv2.java
@@ -10,13 +10,24 @@ public class galibv2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
-
+        float ponts;
         List<Float> liada = new ArrayList<>();
-        do{
-            float melon = sc.nextFloat();
-            liada.add(melon);
-
-        }while(!melon =-1);
-
+        int pos = -1;
+        do {
+            ponts = sc.nextFloat();
+            if (ponts < 0) {
+                break;
+            }
+            liada.add(ponts);
+        } while (ponts > 0);
+        float alturaVehicle = sc.nextFloat();
+        for (int i = 0; i < liada.size(); i++) {
+            if (liada.get(i) < alturaVehicle) {
+                pos = i + 1;
+                System.out.println("xoca amb el pont " + pos);
+                break;
+            }
+        }
+        
     }
 }
