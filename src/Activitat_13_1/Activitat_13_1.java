a6ab575a907b3e4677d8eb4b89c25ee1c7b30171
jmperez <jmperez@elpuig.xeill.net>
2026-04-08 17:01:08 +0200

    Act 13 inicio

--- src/Activitat_13_1/Activitat_13_1.java
+++ src/Activitat_13_1/Activitat_13_1.java
@@ -2,21 +2,73 @@ package Activitat_13_1;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Activitat_13_1 {
+
     public static void main(String[] args) throws IOException {
-     createSaludo();
-     createDirData();
+        //createSaludo();
+        //createDirData();
+        //createDirFecha(nomFechaCarpeta()); // EX4.1
+        ex4_2_3();
+
     }
+
+    public static void ex4_2_3() throws IOException {
+        String nomFecha = nomFechaCarpeta();
+        createDirFecha(nomFecha);
+        String segons = nomSegons();
+        String horesDir = nomHoraCarpeta();
+        createFitxer(nomFecha,horesDir,segons);
+    }
+    //Ex4.1 (a) parte de fecha.
+    public static String nomFechaCarpeta(){
+        LocalDateTime fechaDir = LocalDateTime.now();
+        DateTimeFormatter formatoDir = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String fechaformada = fechaDir.format(formatoDir);
+        return fechaformada;
+        // System.out.println(fechaformada);
+
+    }
+    //Ex3.2 (antes Ex1 y Ex2)
     public static void createSaludo() throws IOException {
-        FileWriter melon = new FileWriter("Saludo.txt", true);
+        FileWriter melon = new FileWriter("data/saludo.txt", true);
         melon.write("Hola\n");
         melon.close();
     }
+    // Ex3.1
     public static void createDirData() throws IOException {
         File castana = new File("data");
         castana.mkdir();
     }
-//TODO de momento funciona, revisar ej 3 y sguientes.
+    // Ex4.1 (b)
+    public static void createDirFecha(String fecha) throws IOException {
+        File castana = new File("data/"+fecha);
+        castana.mkdir();
+    }
+
+    //Ex4.2
+    public static String nomHoraCarpeta(){
+        LocalDateTime horaDir = LocalDateTime.now();
+        DateTimeFormatter formatoDir = DateTimeFormatter.ofPattern("HHmm");
+        String horaFormada = horaDir.format(formatoDir);
+        return horaFormada;
+        // System.out.println(fechaformada);
+    }
+    public static String nomSegons(){
+        LocalDateTime horaDir = LocalDateTime.now();
+        DateTimeFormatter formatoDir = DateTimeFormatter.ofPattern("ss");
+        String horaFormada = horaDir.format(formatoDir);
+        return horaFormada;
+        // System.out.println(fechaformada);
+    }
+
+    public static void createFitxer(String fecha, String hora, String segons) throws IOException {
+        FileWriter melon = new FileWriter("data/"+fecha+"/"+hora+".txt", true);
+        melon.write(segons+"\n");
+        melon.close();
+    }
+
+
 }
